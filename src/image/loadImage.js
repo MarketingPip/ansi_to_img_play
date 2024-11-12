119f8f4ad4d3d4dab22d45bb0c27f7c0ef8cf325
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2024-11-12 18:54:35 -0500

    Create loadImage.js

---
+++ src/image/loadImage.js
@@ -0,0 +1,28 @@
+import _canvas from "canvas";
+
+/**
+ * Loads an image from a given path and returns its image data, width, and height.
+ * @param {string} path - The path to the image file.
+ * @returns {Promise<Object>} Resolves with image data, width, and height.
+ * @throws {Error} Throws an error if the image cannot be loaded.
+ */
+export async function loadImage(path) {
+    const img = await _canvas.loadImage(path);
+    const canvas = _canvas.createCanvas();
+    const ctx = canvas.getContext('2d');
+
+    return new Promise((resolve, reject) => {
+        img.onload = () => {
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx.drawImage(img, 0, 0);
+            resolve({
+                imageData: ctx.getImageData(0, 0, img.width, img.height),
+                width: img.width,
+                height: img.height
+            });
+        };
+        img.onerror = reject;
+        img.crossOrigin = 'anonymous'; 
+    });
+}
