c83db4b38adc0228676bc2efb071d033647aa44e
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2024-11-12 19:00:04 -0500

    Update colorUtils.js

--- src/color/colorUtils.js
+++ src/color/colorUtils.js
@@ -1,3 +1,4 @@
+import { COLOR_MODE } from '../config';
 /**
  * Converts RGB values to the closest 256-color code.
  * @param {number} r - Red value (0-255).
@@ -16,5 +17,26 @@ export function rgbTo256(r, g, b) {
         if (r < 8) return 16;
         if (r > 248) return 231;
         return Math.round(((r - 8) / 247) * 24) + 232;
+    }
+}
+
+/**
+ * Returns the color code for the given RGB values and color mode.
+ *
+ * @param {number} r - The red component of the color (0-255).
+ * @param {number} g - The green component of the color (0-255).
+ * @param {number} b - The blue component of the color (0-255).
+ * @param {string} colorMode - The color mode, either `COLOR_MODE.TRUE_COLOR` or `COLOR_MODE.256`.
+ * @returns {string} The ANSI color code for the given RGB values and color mode.
+ *
+ * @example
+ * const colorCode = getColorCode(255, 0, 0, COLOR_MODE.TRUE_COLOR);
+ * console.log(colorCode); // Outputs the ANSI color code for bright red in true color mode.
+ */
+function getColorCode(r, g, b, colorMode) {
+    if (colorMode === COLOR_MODE.TRUE_COLOR) {
+        return ANSI.bgRGB(r, g, b);
+    } else {
+        return ANSI.bg256(rgbTo256(r, g, b));
     }
 }
