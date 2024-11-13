4c95004e9203d5c467422d62487eeda94d666186
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2024-11-12 21:09:41 -0500

    Update license.config.js

--- src/license.config.js
+++ src/license.config.js
@@ -15,7 +15,7 @@ const LICENSE = `/**!
  * MORE INFO CAN BE FOUND AT https://github.com/MarketingPipeline/${REPONAME}/
  */`;
 
-const FILENAME = "index6"; // used for output file name
+const FILENAME = "index7"; // used for output file name
 
 const packageCONFIG = {LICENSE,FILENAME};
 
